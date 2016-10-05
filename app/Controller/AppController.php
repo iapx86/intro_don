59a81ad15880dbc3ba20ac6cf820425dabce8c42
a01 <a01@julyinc.com>
2016-10-05 12:33:15 +0900

    自分の個所でエラー出たので回避 一度コメントアウトします。

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,27 +32,27 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 	
-	    public $components = array(
-        'Flash',
-        'Auth' => array(
-            'loginRedirect' => array(
-                'controller' => 'posts',
-                'action' => 'index'
-            ),
-            'logoutRedirect' => array(
-                'controller' => 'pages',
-                'action' => 'display',
-                'home'
-            ),
-            'authenticate' => array(
-                'Form' => array(
-                    'passwordHasher' => 'Blowfish'
-                )
-            )
-        )
-    );
+	   //  public $components = array(
+    //     'Flash',
+    //     'Auth' => array(
+    //         'loginRedirect' => array(
+    //             'controller' => 'posts',
+    //             'action' => 'index'
+    //         ),
+    //         'logoutRedirect' => array(
+    //             'controller' => 'pages',
+    //             'action' => 'display',
+    //             'home'
+    //         ),
+    //         'authenticate' => array(
+    //             'Form' => array(
+    //                 'passwordHasher' => 'Blowfish'
+    //             )
+    //         )
+    //     )
+    // );
 
-    public function beforeFilter() {
-        $this->Auth->allow('index', 'view');
+    // public function beforeFilter() {
+    //     $this->Auth->allow('index', 'view');
 
 }
